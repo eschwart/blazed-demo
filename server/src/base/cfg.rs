5fa3c36fe265f1faec44e23953a7f9081b3f6084
splurf <evandotschwartzentruber@gmail.com>
2024-11-25 11:29:31 -0500

    fixed `tps` bounds

--- server/src/base/cfg.rs
+++ server/src/base/cfg.rs
@@ -1,7 +1,10 @@
 use std::time::Duration;
 
+use clap::value_parser;
+
 use crate::{clap::Parser, *};
 
+/// Calculates the duration of a single tick.
 fn calc_tps(tps: u8) -> Duration {
     Duration::from_secs_f32((1000.0 / tps as f32) / 1000.0)
 }
@@ -16,7 +19,7 @@ pub struct Config {
     #[arg(short, long)]
     udp_addr: SocketAddr,
 
-    #[arg(long, default_value_t = 128)]
+    #[arg(long, default_value_t = 128, value_parser = value_parser!(u8).range(1..))]
     tps: u8,
 }
 
